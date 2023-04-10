9398fbe12e18e4e803053e877405e070c6e9d03a
Thanh Dang <dang.hong.thanh@sun-asterisk.com>
2023-04-10 04:16:41 +0000

    edit main file

--- strategies/recent_strategies.py
+++ strategies/recent_strategies.py
@@ -96,41 +96,24 @@ def highest_uncertainty_selection(model,unlabeled_samples,budget):
     queried_samples=unlabeled_samples[idxs[-budget:]]
     return queried_samples
 
-@torch.no_grad()
-def least_confidence_selection(model,unlabeled_samples,budget):
-    unlabeled_set=ActivePolybDataset(unlabeled_samples,transform=semi_transform)
-    unlabeled_loader=torch.utils.data.DataLoader(unlabeled_set, batch_size=1, shuffle=False, num_workers=2)
-    model.eval()
-    model.to(device)
-    confidence_scores=[]
-    for index, batch in enumerate(tqdm(unlabeled_loader)):
-        images = batch['image'].to(device)
-        logits=model(images)
-        probs=torch.sigmoid(logits)
-        class_probs=torch.max(probs,1-probs)
-        mean_probs=torch.sum(class_probs,dim=[-1,-2])/(logits.shape[-1]*logits.shape[-2])
-        confidence_scores.append(mean_probs.cpu().numpy().item())
-    idxs=np.argsort(confidence_scores)
-    unlabeled_samples=np.array(unlabeled_samples)
-    queried_samples=unlabeled_samples[idxs[:budget]]
-    return queried_samples
-@torch.no_grad()
-def mc_dropout_selection(model,unlabeled_samples,budget,device='cuda:0',n_drop=5):
-    enable_dropout(model)
-    model.to(device)
-    unlabeled_set=ActivePolybDataset(unlabeled_samples,transform=semi_transform)
-    unlabeled_loader=torch.utils.data.DataLoader(unlabeled_set, batch_size=1, shuffle=False, num_workers=2)
-    confidence_scores=[]
-    for index, batch in enumerate(tqdm(unlabeled_loader), start=1):
-        images = batch['image'].to(device)
-        probs = model.forward_dropout(images, n_drop=n_drop)
-        class_probs=torch.max(probs,1-probs)
-        mean_probs=torch.sum(class_probs,dim=[-1,-2])/(probs.shape[-1]*probs.shape[-2])
-        confidence_scores.append(mean_probs.cpu().numpy().item())
-    idxs=np.argsort(confidence_scores)
-    unlabeled_samples=np.array(unlabeled_samples)
-    queried_samples=unlabeled_samples[idxs[:budget]]
-    return queried_samples
+
+# @torch.no_grad()
+# def mc_dropout_selection(model,unlabeled_samples,budget,device='cuda:0',n_drop=5):
+#     enable_dropout(model)
+#     model.to(device)
+#     unlabeled_set=ActivePolybDataset(unlabeled_samples,transform=semi_transform)
+#     unlabeled_loader=torch.utils.data.DataLoader(unlabeled_set, batch_size=1, shuffle=False, num_workers=2)
+#     confidence_scores=[]
+#     for index, batch in enumerate(tqdm(unlabeled_loader), start=1):
+#         images = batch['image'].to(device)
+#         probs = model.forward_dropout(images, n_drop=n_drop)
+#         class_probs=torch.max(probs,1-probs)
+#         mean_probs=torch.sum(class_probs,dim=[-1,-2])/(probs.shape[-1]*probs.shape[-2])
+#         confidence_scores.append(mean_probs.cpu().numpy().item())
+#     idxs=np.argsort(confidence_scores)
+#     unlabeled_samples=np.array(unlabeled_samples)
+#     queried_samples=unlabeled_samples[idxs[:budget]]
+#     return queried_samples
 # Loss function and optimizer
 # def entropy_2d(p, dim=0, keepdim=False):
 #     """ 
@@ -187,7 +170,25 @@ def bvsb_selection(model,labeled_flages,train_dataset,budget):
     labeled_flages[query_idxs]=True
     return query_idxs,labeled_flages
 @torch.no_grad()
-def mc_dropout_selection_true(model,unlabeled_samples,budget,device='cuda:0',n_drop=10):
+def least_confidence_selection(model,unlabeled_samples,budget):
+    unlabeled_set=ActivePolybDataset(unlabeled_samples,transform=semi_transform)
+    unlabeled_loader=torch.utils.data.DataLoader(unlabeled_set, batch_size=1, shuffle=False, num_workers=2)
+    model.eval()
+    model.to(device)
+    confidence_scores=[]
+    for index, batch in enumerate(tqdm(unlabeled_loader)):
+        images = batch['image'].to(device)
+        logits=model(images)
+        probs=torch.sigmoid(logits)
+        class_probs=torch.max(probs,1-probs)
+        mean_probs=torch.sum(class_probs,dim=[-1,-2])/(logits.shape[-1]*logits.shape[-2])
+        confidence_scores.append(mean_probs.cpu().numpy().item())
+    idxs=np.argsort(confidence_scores)
+    unlabeled_samples=np.array(unlabeled_samples)
+    queried_samples=unlabeled_samples[idxs[:budget]]
+    return queried_samples
+@torch.no_grad()
+def mc_dropout_selection(model,unlabeled_samples,budget,device='cuda:0',n_drop=10):
     model.train()
     model.to(device)
     unlabeled_set=ActivePolybDataset(unlabeled_samples,transform=semi_transform)
